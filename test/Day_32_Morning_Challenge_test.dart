069bd5601eacfeb83f2306335e9763e378db25b1
Ashish <ashishteddy2@gmail.com>
2019-11-18 14:35:01 +0530

    change 3.0

--- test/Day_32_Morning_Challenge_test.dart
+++ test/Day_32_Morning_Challenge_test.dart
@@ -4,14 +4,21 @@ import 'package:test/test.dart';
 import '../bin/main.dart';
 
 void main() {
-  Shiritori game1 = Shiritori();
+  Shiritori game = Shiritori();
   test('Shiritori', () {
-    expect(game1.play("hello"),true);
-    expect(game1.play("orange"),true);
-    expect(game1.play("eagle"),true);
-    expect(game1.play("ear"),true);
-    expect(game1.play("camel"),false);
-    game1.restart();
-    expect(game1.play("hello"),true);
+    expect(game.play("hellO"),true);
+    expect(game.play("orange"),true);
+    expect(game.words.isEmpty ,false);
+    expect(game.play("cat"),false);
+    expect(game.game_over,true);
+    game.restart();
+    expect(game.words.isEmpty,true);
+    expect(game.game_over, false);
+    expect(game.play("ear"),true);
+    expect(game.play("ear"),false);
+    expect(game.words.length == 1,true);
+    game.restart();
+    expect(game.play("hello"),true);
+    //TODO gameover, repetition, return value of reatart, words list size
   });
 }

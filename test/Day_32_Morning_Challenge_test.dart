ac68e29cec74053cc6e33e68eb7741acd330a58c
Ashish <ashishteddy2@gmail.com>
2019-11-18 11:56:35 +0530

    change 2.5

--- test/Day_32_Morning_Challenge_test.dart
+++ test/Day_32_Morning_Challenge_test.dart
@@ -4,29 +4,13 @@ import 'package:test/test.dart';
 import '../bin/main.dart';
 
 void main() {
-  String name = 'Ashish';
-  print(name.length);
-  print('hello');
-  test('Tes11t if words are chained properly', (){
-    Shiritori shirotori = Shiritori();
-
-    expect(shirotori.words[shirotori.words.length].substring(shirotori.words.length) == shirotori.words[shirotori.words.length-1].substring(0), true);
+  Shiritori game1=Shiritori();
+  test('Shiritori', () {
+    expect(game1.play("hello"),true);
+    expect(game1.play("orange"),true);
+    expect(game1.play("eagle"),true);
+    expect(game1.play("ear"),true);
+    expect(game1.play("camel"),false);
+    game1.restart();
+    expect(game1.play("hello"),true);
   });
-  test('Test if words are not repeated', (){
-    Shiritori shirotori = Shiritori();
-    shirotori.words = ['apple', 'cabbage', 'shrimp', 'doom', 'doom'];
-    expect(shirotori.words.length == shirotori.words.toSet().length, true);
-  });
-  test('Check if game over is working', (){
-    Shiritori shirotori = Shiritori();
-    shirotori.words = ['apple', 'cabbage', 'shrimp', 'doom', 'doom'];
-    expect((shirotori.words.length != shirotori.words.toSet().length && shirotori.game_over == true), true);
-  });
-  test('Test if restart is working', (){
-    Shiritori shirotori = Shiritori();
-    shirotori.words = ['apple', 'cabbage', 'shrimp', 'doom', 'doom'];
-    shirotori.game_over = true;
-    shirotori.restart();
-    expect((shirotori.words == [] && shirotori.game_over == false), true);
-  });
-}

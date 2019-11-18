9d60df1977c73efcdb07e1e8d945fd54a584ba34
Ashish <ashishteddy2@gmail.com>
2019-11-18 09:13:30 +0530

    change 1.1

--- bin/main.dart
+++ bin/main.dart
@@ -15,6 +15,7 @@
 // (the word should follow rules #1 and #2 above).
 // - restart: a method that sets the words list to an empty one [] and sets the
 // game_over boolean to false. It should return "game restarted".
+
 main() {
-  print('hello')
-;}
+
+}
--- test/Day_32_Morning_Challenge_test.dart
+++ test/Day_32_Morning_Challenge_test.dart
@@ -1,5 +1,8 @@
 import 'package:test/test.dart';
 
 void main() {
+  print('hello');
+  test('Test if words are chained properly', (){
 
+  });
 }
